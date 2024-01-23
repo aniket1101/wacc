c8a15e18ca5dc3412cb1dac7b81c24efb639b6cc
Alexander Shemaly <as7322@willow10.doc.ic.ac.uk>
2024-01-23 16:18:40 +0000

    wacc_examples: Added example wacc programs. Main.scala: Modified main() to take in a filename, rather than the program as a string.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,15 +1,29 @@
 import parsley.{Success, Failure}
+import scala.io.Source
 
 object Main {
     def main(args: Array[String]): Unit = {
-        println("hello WACC!")
-
         args.headOption match {
-            case Some(expr) => parser.parse(expr) match {
-                case Success(x) => println(s"$expr = $x")
-                case Failure(msg) => println(msg)
-            }
-            case None => println("please enter an expression")
+            case Some(filename) =>
+                val fileContent = readFile(filename)
+                parser.parse(fileContent) match {
+                    case Success(x) => println(s"$fileContent = $x")
+                    case Failure(msg) => println(msg)
+                }
+            case None => println("No file passed in.")
+        }
+    }
+
+    private def readFile(filename: String): String = {
+        try {
+            val source = Source.fromFile(filename)
+            val content = source.mkString
+            source.close()
+            content
+        } catch {
+            case e: Exception =>
+                println(s"Error reading file $filename: ${e.getMessage}.")
+                sys.exit(1)
         }
     }
 }
