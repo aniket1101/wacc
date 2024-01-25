488f5e9957e1f45d39412c961ce348a56dac1608
aniket1101 <1.aniketgupta@gmail.com>
2024-01-25 21:39:15 +0000

    lexer.scala: Imported correct parsley modules to create lexical description

--- src/main/scala/lexer.scala
+++ src/main/scala/lexer.scala
@@ -1,7 +1,9 @@
 import parsley.Parsley
-import parsley.token.descriptions._
-import parsley.token.symbol.ImplicitSymbol
-import parsley.token.{Lexer, predicate}
+import parsley.token.Lexer
+import parsley.token.predicate.Basic
+import parsley.token.descriptions.{LexicalDesc, NameDesc, SymbolDesc, SpaceDesc}
+import parsley.token.descriptions.numeric.{NumericDesc, ExponentDesc}
+import parsley.token.descriptions.text.{TextDesc, EscapeDesc}
 
 object lexer {
     final val keywords = Set(
@@ -61,8 +63,8 @@ object lexer {
 
     private val desc = LexicalDesc.plain.copy(
         nameDesc = NameDesc.plain.copy(
-            identifierStart = predicate.Basic(_.isLetter),
-            identifierLetter = predicate.Basic(_.isLetterOrDigit),
+            identifierStart = Basic(_.isLetter),
+            identifierLetter = Basic(_.isLetterOrDigit),
         ),
         symbolDesc = SymbolDesc.plain.copy(
             hardKeywords = keywords,
@@ -73,33 +75,32 @@ object lexer {
             escapeSequences = EscapeDesc.plain.copy(
                 escBegin = '\\',
                 literals = Set.empty,
-                singleMap = Map(
-                    '0' -> 0x0000,
-                    'b' -> 0x0008,
-                    't' -> 0x0009,
-                    'n' -> 0x000a,
-                    'f' -> 0x000c,
-                    'r' -> 0x000d,
-                    '"' -> 0x0022,
-                    '\'' -> 0x0027,
-                    '\\' -> 0x005c
-                ),
-                graphicCharacter = predicate.Basic(c => {
-                    (c != '"') && (c != '\'') && (c != '\\') && (c != '\n') && (c != '\t') && (c != '\r') && (c != '\f') && (c != '\b')
-                })
-            )
+                mapping = Map(
+                    "0" -> 0x0000,
+                    "b" -> 0x0008,
+                    "t" -> 0x0009,
+                    "n" -> 0x000a,
+                    "f" -> 0x000c,
+                    "r" -> 0x000d,
+                    "\""-> 0x0022,
+                    "\'" -> 0x0027,
+                    "\\" -> 0x005c
+                )
+            ),
+            graphicCharacter = Basic(c => {
+                (c != '"') && (c != '\'') && (c != '\\') && (c != '\n') && (c != '\t') && (c != '\r') && (c != '\f') && (c != '\b')
+            })
         ),
         spaceDesc = SpaceDesc.plain.copy(
-            commentLine = "#",
-            space = predicate.Basic(_.isWhitespace)
+            lineCommentStart = "#"
         )
     )
-    
+
     private val lexer = new Lexer(desc)
 
     def fully[A](p: Parsley[A]): Parsley[A] = lexer.fully(p)
     val ident: Parsley[String] = lexer.lexeme.names.identifier
     val nat: Parsley[BigInt] = lexer.lexeme.unsigned.decimal
 
-    val implicits: ImplicitSymbol = lexer.lexeme.symbol.implicits
+    val implicits = lexer.lexeme.symbol.implicits
 }
