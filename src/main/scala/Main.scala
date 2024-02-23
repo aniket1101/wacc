e34da86b44f107432cf383bca8facf85b24fe0ee
Alexander Shemaly <as7322@texel26.doc.ic.ac.uk>
2024-02-23 15:40:13 +0000

    [as7322] [ag2922] Main.scala: Removed print statement case to be temporarily unhandled

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -16,55 +16,6 @@ object Main {
   val SYNTAX_ERROR_EXIT_STATUS: Int = 100
   val SEMANTIC_ERROR_EXIT_STATUS: Int = 200
   private val FAIL: Int = -1
-  val helloWorldProgram =
-    """.intel_syntax noprefix
-      |.globl main
-      |.section .rodata
-      |# length of .L.str0
-      |	.int 12
-      |.L.str0:
-      |	.asciz "Hello World!"
-      |.text
-      |main:
-      |	push rbp
-      |	push rbx
-      |	mov rbp, rsp
-      |	# Stack pointer unchanged, no stack allocated arguments
-      |	lea rax, [rip + .L.str0]
-      |	push rax
-      |	pop rax
-      |	mov rax, rax
-      |	mov rdi, rax
-      |	# statement primitives do not return results (but will clobber r0/rax)
-      |	call _prints
-      |	mov rax, 0
-      |	pop rbx
-      |	pop rbp
-      |	ret
-      |
-      |.section .rodata
-      |# length of .L._prints_str0
-      |	.int 4
-      |.L._prints_str0:
-      |	.asciz "%.*s"
-      |.text
-      |_prints:
-      |	push rbp
-      |	mov rbp, rsp
-      |	# external calls must be stack-aligned to 16 bytes, accomplished by masking with fffffffffffffff0
-      |	and rsp, -16
-      |	mov rdx, rdi
-      |	mov esi, dword ptr [rdi - 4]
-      |	lea rdi, [rip + .L._prints_str0]
-      |	# on x86, al represents the number of SIMD registers used as variadic arguments
-      |	mov al, 0
-      |	call printf@plt
-      |	mov rdi, 0
-      |	call fflush@plt
-      |	mov rsp, rbp
-      |	pop rbp
-      |	ret
-      |""".stripMargin
 
   // Main function of the program
   def main(args: Array[String]): Unit = {
@@ -112,33 +63,18 @@ object Main {
     }
   }
 
-  def isPrintHelloWorld(prog: Prog): Boolean = {
-    val stmts = prog.stats
-    stmts.exists { stmt =>
-      stmt match {
-        case Print(x) => true
-        case _ => false
-      }
-    }
-  }
-
   def compileProgram(source: String): Int = {
     val file = new File(source)
     parseProgram(file) match {
       case Left(exitCode) => exitCode
       case Right((prog, symbolTable)) =>
-        if (isPrintHelloWorld(prog)) {
-          writeToFile(helloWorldProgram, removeFileExt(file.getName) + ".s")
-          VALID_EXIT_STATUS
-        } else {
-          val asmInstr = IRTranslator.translateAST(prog, symbolTable)
-          val asmCode = new IntelX86Translator().toAsmCode(asmInstr)
-          writeToFile(asmCode, removeFileExt(file.getName) + ".s") match {
-            case VALID_EXIT_STATUS => VALID_EXIT_STATUS
-            case err =>
-              println("Failed to write to output file")
-              err
-          }
+        val asmInstr = IRTranslator.translateAST(prog, symbolTable)
+        val asmCode = new IntelX86Translator().toAsmCode(asmInstr)
+        writeToFile(asmCode, removeFileExt(file.getName) + ".s") match {
+          case VALID_EXIT_STATUS => VALID_EXIT_STATUS
+          case err =>
+            println("Failed to write to output file")
+            err
         }
     }
   }
