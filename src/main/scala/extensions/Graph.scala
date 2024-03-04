df0bac4db54dbaf40267572e813223fa72b3cafe
Alexander Balinsky <ab3722@aube.doc.ic.ac.uk>
2024-03-04 13:55:59 +0000

    Graph.scala: rest of implementation

--- src/main/scala/extensions/Graph.scala
+++ src/main/scala/extensions/Graph.scala
@@ -1,5 +1,59 @@
 package extensions
 
+import scala.collection.mutable
+
+case class Edge(string: String)
+
 class Graph {
+    private val adjacencyList: mutable.HashMap[String, mutable.ListBuffer[String]] = mutable.HashMap()
+
+    def addVertex(vertex: String): Unit = {
+      if (!adjacencyList.contains(vertex)) {
+        adjacencyList(vertex) = mutable.ListBuffer[String]()
+      }
+    }
+
+    def addEdge(source: String, destination: String): Unit = {
+      addVertex(source)
+      addVertex(destination)
+      adjacencyList(source).append(destination)
+    }
+
+    def getVertices: Iterable[String] = {
+      adjacencyList.keys
+    }
+
+    def getNeighbors(vertex: String): Iterable[String] = {
+      adjacencyList.getOrElse(vertex, mutable.ListBuffer())
+    }
+
+    override def toString: String = {
+      val str = new StringBuilder()
+      for ((vertex, neighbors) <- adjacencyList) {
+        str.append(s"$vertex -> ${neighbors.mkString(", ")}\n")
+      }
+      str.toString()
+  }
+
+  def dfsUtil(start: String, visited: Option[mutable.Set[String]]): Option[mutable.Set[String]] = {
+    visited match {
+      case Some(visitedSet) =>
+        visitedSet.add(start)
+        for (neighbor <- getNeighbors(start)) {
+          if (visitedSet.contains(neighbor)) {
+            None
+          }
+          dfsUtil(neighbor, Some(visitedSet))
+        }
+        None
+      case None => None
+    }
+  }
+
+  def dfs(start: String): Option[mutable.Set[String]] = {
+    val visited = Some(mutable.Set[String]())
+    dfsUtil(start, visited)
+  }
+
 
 }
